d0665af4c56f75b38c5985c35774561707af99e6
David-Pops <dpopowski6371@stu.d214.org>
2023-09-18 11:16:59 -0500

    starting

--- binary to hexadecimal/binary stuff.swift
+++ binary to hexadecimal/binary stuff.swift
@@ -9,18 +9,25 @@ import SwiftUI
 import CoreData
 
 struct binary_stuff: View {
+    
+    @State private var binaryNumber:String = ""
+    
     var body: some View {
-        //TextField("binary number", text: <#T##Binding<String>#>)
-        
-        
-        
+        VStack{
+            //TextField("binary number", text: $binaryNumber)
+            
+            TextField(text: $binaryNumber, prompt: Text("Required")) {
+                
+            }
+            Text(\( Int(binaryNumber) ?? 0))
+        }
     }
 }
 
 
 struct binary_stuff_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        binary_stuff().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
 
