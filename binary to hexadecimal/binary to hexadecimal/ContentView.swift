0a2362754671783e79035f2b584acde618b78e48
letiiita <lrios4400@stu.d214.org>
2023-09-20 10:51:05 -0500

    decimal to hex

--- binary to hexadecimal/binary to hexadecimal/ContentView.swift
+++ binary to hexadecimal/binary to hexadecimal/ContentView.swift
@@ -7,17 +7,43 @@
 
 import SwiftUI
 import CoreData
+extension String {
+    var hexadecimalRepresentation: String {
+        let decimal = Int(self)
+        return String(format: "%X", decimal ?? 0)
+    }
+}
 
 struct ContentView: View {
-    @State private var numero: String = "0000"
+    @State private var numero: String = "1"
+    @State private var decimalNumber: Double = 0.0
+    
+    var hexadecimalString: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 0
+        formatter.locale = Locale.current
+        formatter.maximumIntegerDigits = 20 
+
+        if let decimalNumber = Decimal(string: "\(self.decimalNumber)") {
+            return formatter.string(from: decimalNumber as NSDecimalNumber)?.hexadecimalRepresentation ?? ""
+        } else {
+            return ""
+        }
+    }
+
 
     var body: some View {
         VStack() {
-            TextField("enter number", text: $numero)
-           
-            if numero == "000" {
+            TextField("Enter Decimal Number", value: $decimalNumber, formatter: NumberFormatter())
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+
+                        Text("Hexadecimal: \(hexadecimalString)")
+                            .padding()
                
-            }
+            
         }
     }
 }
